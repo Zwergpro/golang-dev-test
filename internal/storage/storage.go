e90326edbbd87b5afa95147168b46e5b00f02163
Нестеренко Сергей <nestsdev@gmail.com>
2022-07-24 16:52:44 +0300

    [GOHW-2]: add RWMutex to Warehouse storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,15 +4,26 @@ import (
 	"github.com/pkg/errors"
 	"log"
 	"strconv"
+	"sync"
 )
 
-var warehouse map[uint64]*Product
+type Warehouse struct {
+	mu      sync.RWMutex
+	storage map[uint64]*Product
+}
 
-var ProductAlreadyExists = errors.New("product already exists")
-var ProductNotExists = errors.New("product does not exist")
+var warehouse Warehouse
+
+var (
+	ProductAlreadyExists = errors.New("product already exists")
+	ProductNotExists     = errors.New("product does not exist")
+)
 
 func init() {
-	warehouse = make(map[uint64]*Product)
+	warehouse = Warehouse{
+		storage: make(map[uint64]*Product),
+	}
+
 	product, _ := NewProduct("pillow", 500, 10)
 	if err := Add(product); err != nil {
 		log.Fatal(err)
@@ -20,40 +31,55 @@ func init() {
 }
 
 func Get(id uint64) (*Product, error) {
-	product, ok := warehouse[id]
+	warehouse.mu.RLock()
+	defer warehouse.mu.RUnlock()
+
+	product, ok := warehouse.storage[id]
 	if ok {
 		return product, nil
 	}
-	return nil, errors.Wrap(ProductNotExists, strconv.FormatUint(uint64(id), 10))
+	return nil, errors.Wrap(ProductNotExists, strconv.FormatUint(id, 10))
 }
 
 func Add(p *Product) error {
-	if _, ok := warehouse[p.GetId()]; ok {
-		return errors.Wrap(ProductAlreadyExists, strconv.FormatUint(uint64(p.GetId()), 10))
+	warehouse.mu.Lock()
+	defer warehouse.mu.Unlock()
+
+	if _, ok := warehouse.storage[p.GetId()]; ok {
+		return errors.Wrap(ProductAlreadyExists, strconv.FormatUint(p.GetId(), 10))
 	}
-	warehouse[p.GetId()] = p
+	warehouse.storage[p.GetId()] = p
 	return nil
 }
 
 func Delete(id uint64) error {
-	if _, ok := warehouse[id]; !ok {
+	warehouse.mu.Lock()
+	defer warehouse.mu.Unlock()
+
+	if _, ok := warehouse.storage[id]; !ok {
 		return errors.Wrap(ProductNotExists, strconv.FormatUint(id, 10))
 	}
-	delete(warehouse, id)
+	delete(warehouse.storage, id)
 	return nil
 }
 
 func Update(p *Product) error {
-	if _, ok := warehouse[p.GetId()]; !ok {
-		return errors.Wrap(ProductNotExists, strconv.FormatUint(uint64(p.GetId()), 10))
+	warehouse.mu.Lock()
+	defer warehouse.mu.Unlock()
+
+	if _, ok := warehouse.storage[p.GetId()]; !ok {
+		return errors.Wrap(ProductNotExists, strconv.FormatUint(p.GetId(), 10))
 	}
-	warehouse[p.GetId()] = p
+	warehouse.storage[p.GetId()] = p
 	return nil
 }
 
 func List() []*Product {
-	products := make([]*Product, 0, len(warehouse))
-	for _, v := range warehouse {
+	warehouse.mu.RLock()
+	defer warehouse.mu.RUnlock()
+
+	products := make([]*Product, 0, len(warehouse.storage))
+	for _, v := range warehouse.storage {
 		products = append(products, v)
 	}
 	return products
