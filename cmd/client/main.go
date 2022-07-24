62e6336163bc0705e52743cb0acbb49732b03eb5
Нестеренко Сергей <nestsdev@gmail.com>
2022-07-24 19:23:57 +0300

    [GOHW-2]: add test GRPC client

---
+++ cmd/client/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "homework-1/pkg/api/v1"
+	"log"
+)
+
+// Test GRPC client
+func main() {
+	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := pb.NewAdminServiceClient(conn)
+
+	ctx := context.Background()
+	productId := uint64(1)
+
+	if response, err := client.ProductList(ctx, &pb.ProductListRequest{}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("list: %v \n", response)
+	}
+
+	if response, err := client.ProductGet(ctx, &pb.ProductGetRequest{Id: productId}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("get: %v \n", response)
+	}
+
+	createProductRequest := pb.ProductCreateRequest{
+		Name:     "new product",
+		Price:    100,
+		Quantity: 10,
+	}
+	if response, err := client.ProductCreate(ctx, &createProductRequest); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("create: %v \n", response)
+		productId = response.GetId()
+	}
+
+	if response, err := client.ProductList(ctx, &pb.ProductListRequest{}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("list: %v \n", response)
+	}
+
+	updateProductRequest := pb.ProductUpdateRequest{
+		Id:       productId,
+		Name:     "New name",
+		Price:    20,
+		Quantity: 30,
+	}
+	if response, err := client.ProductUpdate(ctx, &updateProductRequest); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("update: %v \n", response)
+	}
+
+	if response, err := client.ProductList(ctx, &pb.ProductListRequest{}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("list: %v \n", response)
+	}
+
+	if response, err := client.ProductDelete(ctx, &pb.ProductDeleteRequest{Id: productId}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("delete: %v \n", response)
+	}
+
+	if response, err := client.ProductList(ctx, &pb.ProductListRequest{}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("list: %v \n", response)
+	}
+}
